fb1685e078ffc0e58db135aca2c93480213421a4
heeyonhh <lightram23@gmail.com>
2023-08-09 19:48:08 +0900

    intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,6 +7,7 @@ import { Nav } from "./data";
 
 const Intro = () => {
     const [elementsOpacity, setElementsOpacity] = useState(0);
+    const [animateState, setAnimateState] = useState(false);
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
@@ -14,35 +15,44 @@ const Intro = () => {
         gsap.to(".intro_bg", {
             duration: 2,
             height: 0,
-            delay: 2, //2초후 시작
+            delay: 2,
             ease: "power2.inOut",
             onComplete: () => {
-                setElementsOpacity(1); // 요소들을 보이게 설정
+                setElementsOpacity(1);
             },
         });
-        
+
+        const handleScroll = () => {
+            setAnimateState(window.scrollY >= window.innerHeight * 0.5);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+
     }, []);
 
     return (
         <>
             <div className="intro_bg"></div>
-            <div id="intro" className="item">
-                <div className="intro_top">
-                    <span className='intro_name' style={{ opacity: elementsOpacity }}>
+            <div id="intro" className={`item ${animateState ? "animate" : ""}`}>
+                <div className={`intro_top ${animateState ? "animate" : ""}`}>
+                    <span className={`intro_name ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
                         Heeyon Kim
                     </span>
                 </div>
-                <div className="intro_wrap">
-                    <p className="intro_text_item" style={{ opacity: elementsOpacity }}>
+                <div className='intro_wrap'>
+                    <p className={`intro_text_item ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
                         Constantly<br />evolving,<br />
                         New<br />front-end<br />developers.
                     </p>
                 </div>
-                <nav className="nav" style={{ opacity: elementsOpacity }}>
+                <nav className={`nav ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
                     <ul>
                         {Nav.map((nav, i) => (
-                            <li key={i}>
-                                <Link to={nav.id}>
+                            <li className={`menu_box ${animateState ? "animate" : ""}`} key={i}>
+                                <Link className={`menu ${animateState ? "animate" : ""}`} to={nav.id}>
                                     {nav.title}
                                 </Link>
                             </li>
