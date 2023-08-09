7fe29c0a9264c64e8a087e2df02459f3cbbdb7ab
heeyonhh <lightram23@gmail.com>
2023-08-09 17:28:12 +0900

    intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,12 +6,39 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Nav } from "./data";
 
 const Intro = () => {
+    const [elementsOpacity, setElementsOpacity] = useState(0);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(".intro_bg", {
+            duration: 2,
+            height: 0,
+            delay: 2, //2초후 시작
+            ease: "power2.inOut",
+            onComplete: () => {
+                setElementsOpacity(1); // 요소들을 보이게 설정
+            },
+        });
+        
+    }, []);
 
     return (
         <>
             <div className="intro_bg"></div>
             <div id="intro" className="item">
-                <nav className="nav">
+                <div className="intro_top">
+                    <span className='intro_name' style={{ opacity: elementsOpacity }}>
+                        Heeyon Kim
+                    </span>
+                </div>
+                <div className="intro_wrap">
+                    <p className="intro_text_item" style={{ opacity: elementsOpacity }}>
+                        Constantly<br />evolving,<br />
+                        New<br />front-end<br />developers.
+                    </p>
+                </div>
+                <nav className="nav" style={{ opacity: elementsOpacity }}>
                     <ul>
                         {Nav.map((nav, i) => (
                             <li key={i}>
@@ -22,19 +49,6 @@ const Intro = () => {
                         ))}
                     </ul>
                 </nav>
-                <div className="intro_wrap" ref={introTextRef}>
-                     <p className="intro_text_item">
-                        Constantly<br />evolving,<br />
-                        New<br />front-end<br />developers.
-                    </p>
-                </div>
-                <div className="intro_bottom">
-                    <span
-                        className='intro_name'
-                    >
-                        Heeyon Kim
-                    </span>
-                </div>
             </div>
         </>
     );
