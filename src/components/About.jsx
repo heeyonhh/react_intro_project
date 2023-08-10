6c5ea56629713346cf12c24e19af0c033f831229
heeyonhh <lightram23@gmail.com>
2023-08-10 13:15:40 +0900

    map

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import about_img from "../assets/img/aboutimg.jpg"
+import about_img from "../assets/img/aboutimg.jpg";
+import { aboutData } from "./data";
 
 const About = () => {
 
@@ -39,22 +40,20 @@ const About = () => {
           <img src={about_img} alt="about_img" className="aniitem" />
         </div>
         <div className="about_right">
-          <div className="about aniitem">About</div>
-          <div className="about_text">
-            <p className="aniitem">Name / 김 희 연 Heeyon Kim</p>
-            <p className="aniitem">Date Of Birth / 1992 08 27</p>
-            <p className="aniitem">Professional Certificates / NCS UI&UX 디자인 프론트엔드 개발자 양성 통합심사 과정수료, 웹디자인 기능사, 컴퓨터 그래픽스 운용기능사, 2종 보통 운전면허</p>
-            <p className="aniitem">Work Experience / AK MALL 패션잡화 AMD, 패션 브랜드 피칸파이 운영</p>
-            <p className="aniitem">Phone / 010 7271 6432</p>
-            <p className="aniitem">Address / 경기도 화성시 봉담</p>
-          </div>
-          <div className="skill aniitem">Skill</div>
-          <div className="about_skill">
-            <p className="aniitem">언어 / HTML CSS Javascript Typescript</p>
-            <p className="aniitem">라이브러리 /  jQuery React ReactNative</p>
-            <p className="aniitem">GitHub MUI Bootstrap Expo Firebase</p>
-            <p className="aniitem">디자인툴 /  Photoshop Illustrator Figma</p>
-          </div>
+          {aboutData.map((about, i) => (
+            <div key={i}>
+              <div className="about_title aniitem">
+                {about.title}
+              </div>
+              <div className="about_text">
+                {about.content.map((item, j) => (
+                  <p className="aniitem" key={j}>
+                    {item}
+                  </p>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/components/data.js
+++ src/components/data.js
@@ -17,6 +17,29 @@ export const Nav = [
     },
 ];
 
+export const aboutData = [
+    {
+        title: 'About',
+        content: [
+            'Name / 김 희 연 Heeyon Kim',
+            'Date Of Birth / 1992 08 27',
+            'Professional Certificates / NCS UI&UX 디자인 프론트엔드 개발자 양성 통합심사 과정수료, 웹디자인 기능사, 컴퓨터 그래픽스 운용기능사, 2종 보통 운전면허',
+            'Work Experience / AK MALL 패션잡화 AMD, 패션 브랜드 피칸파이 운영',
+            'Phone / 010 7271 6432',
+            'Address / 경기도 화성시 봉담',
+        ],
+    },
+    {
+        title: 'Skill',
+        content: [
+            '언어 / HTML CSS Javascript Typescript',
+            '라이브러리 / jQuery React ReactNative',
+            'GitHub MUI Bootstrap Expo Firebase',
+            '디자인툴 / Photoshop Illustrator Figma',
+        ],
+    },
+];
+
 export const projectData = [
     {
         title: "플라토 커피 주문 앱",
