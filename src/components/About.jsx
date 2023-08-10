eeb43f1a0b0e16452a1af6cb381a7e08d5ef6579
heeyonhh <lightram23@gmail.com>
2023-08-10 10:54:26 +0900

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,52 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import about_img from "../assets/img/aboutimg.jpg"
 
 const About = () => {
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    //이질감 효과 주기
+    gsap.utils.toArray(".about_item").forEach(item => {
+      gsap.to(item, {
+        yPercent: -10, // 위로 10%만큼 이동
+        ease: "none",
+        duration: 1.0, // 지속 시간
+        scrollTrigger: {
+          trigger: item,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 0.4, // 스크롤에 따라 애니메이션 속도를 조절
+        },
+      });
+    });
+
+  }, []);
 
   return (
     <section id="about" className="item" data-bgcolor="#f6f5f2">
       <div className="about_wrap">
         <div className="about_left">
-          <img src={about_img} alt="about_img" />
+          <img src={about_img} alt="about_img" className="about_item"/>
         </div>
         <div className="about_right">
-          <p className="about">About</p>
-          <p className="about_text">
-            Name / 김 희 연 Heeyon Kim<br/>
-            Date Of Birth / 1992 08 27<br/>
-            Professional Certificates / NCS UI&UX 디자인 프론트엔드 개발자 양성 통합심사 과정수료, 웹디자인 기능사, 컴퓨터 그래픽스 운용기능사, 2종 보통 운전면허<br/>
-            Work Experience / AK MALL 패션잡화 AMD, 패션 브랜드 피칸파이 운영<br/>
-            Phone / 010 7271 6432<br/>
+          <p className="about about_item">About</p>
+          <p className="about_text about_item">
+            Name / 김 희 연 Heeyon Kim<br />
+            Date Of Birth / 1992 08 27<br />
+            Professional Certificates / NCS UI&UX 디자인 프론트엔드 개발자 양성 통합심사 과정수료, 웹디자인 기능사, 컴퓨터 그래픽스 운용기능사, 2종 보통 운전면허<br />
+            Work Experience / AK MALL 패션잡화 AMD, 패션 브랜드 피칸파이 운영<br />
+            Phone / 010 7271 6432<br />
             Address / 경기도 화성시 봉담
           </p>
-          <p className="skill">Skill</p>
-          <p className="about_skill">
-            언어 /  HTML CSS Javascript Typescript<br/>
-            라이브러리 /  jQuery React ReactNative<br/>
-            GitHub MUI Bootstrap Expo Firebase<br/>
+          <p className="skill about_item">Skill</p>
+          <p className="about_skill about_item">
+            언어 /  HTML CSS Javascript Typescript<br />
+            라이브러리 /  jQuery React ReactNative<br />
+            GitHub MUI Bootstrap Expo Firebase<br />
             디자인툴 /  Photoshop Illustrator Figma
           </p>
         </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -39,29 +39,29 @@ const Intro = () => {
     return (
         <>
             <div className="intro_bg"></div>
-            <div id="intro"  data-bgcolor="#1c1c1c" className={`item ${animateState ? "animate" : ""}`}>
+            <div id="intro" data-bgcolor="#1c1c1c" className='item'>
                 <div className={`intro_top ${animateState ? "animate" : ""}`}>
-                    <span className={`intro_name ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
-                        Heeyon Kim
-                    </span>
-                </div>
-                <div className='intro_wrap'>
-                    <p className={`intro_text_item ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
+                    <span className={`intro_text ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
                         Constantly<br />evolving,<br />
                         New<br />front-end<br />developers.
+                    </span>
+                    <nav className={`nav ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
+                        <ul>
+                            {Nav.map((nav, i) => (
+                                <li key={i}>
+                                    <Link className={`menu ${animateState ? "animate" : ""}`} to={nav.id}>
+                                        {nav.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+                <div className='intro_wrap'>
+                    <p className={`intro_logo ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
+                        Heeyon Kim
                     </p>
                 </div>
-                <nav className={`nav ${animateState ? "animate" : ""}`} style={{ opacity: elementsOpacity }}>
-                    <ul>
-                        {Nav.map((nav, i) => (
-                            <li className={`menu_box ${animateState ? "animate" : ""}`} key={i}>
-                                <Link className={`menu ${animateState ? "animate" : ""}`} to={nav.id}>
-                                    {nav.title}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
             </div>
         </>
     );
