140a5d6742afdb33a0e111fdce53f92b4c0e6f77
heeyonhh <lightram23@gmail.com>
2023-08-25 15:06:52 +0900

    commit

--- src/components/Ani.jsx
+++ src/components/Ani.jsx
@@ -8,7 +8,7 @@ const Ani = () => {
         <div id="ani" className="item" data-bgcolor="#e5e3db">
             <div className="ani_wrap">
                 <div className="ani">
-                    Constantly evolving, New&nbsp;<img src={footerimg} alt="footerimg" />&nbsp;front-end developer.&nbsp;<img src={footerimg} alt="footerimg" />
+                    Constantly evolving, New&nbsp;<img src={footerimg} alt="footerimg" />&nbsp;front-end developer.&nbsp;<img src={footerimg} alt="footerimg" />&nbsp;Constantly evolving, New&nbsp;<img src={footerimg} alt="footerimg" />&nbsp;front-end developer.&nbsp;<img src={footerimg} alt="footerimg" />
                 </div>
             </div>
         </div>
